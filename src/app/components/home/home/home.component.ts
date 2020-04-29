d92bf7ae6b6e5a9f06a19e48e6e9a677ef5f0a6d
JChiranjeev <cjain252@gmail.com>
2020-04-29 13:08:31 +0530

    Completed Basic Home Screen Layout and Logic

--- src/app/components/home/home/home.component.ts
+++ src/app/components/home/home/home.component.ts
@@ -15,11 +15,17 @@ export class HomeComponent implements OnInit {
 
   user: User;
   bookListItem: Element;
+  noteListItem: Element;
   newBookName: string;
   currentBook: Book;
-  newNoteTitle: string;
-  newNoteContent: string;
+  noteTitle: string;
+  noteContent: string;
   newNoteInputDiv: Element;
+  notesListDiv: Element;
+  discardDisabled: boolean = true;
+  saveDisabled: boolean = true;
+  titleChanged: boolean = false;
+  contentChanged: boolean = false;
 
   constructor(private router: Router, private location: Location, private userController: Usercontroller) { }
 
@@ -34,17 +40,32 @@ export class HomeComponent implements OnInit {
     this.newNoteInputDiv = document.getElementsByClassName("new-note-input")[0];
     this.newNoteInputDiv.setAttribute("hidden",null);
     this.currentBook = this.user.books[0];
+    this.notesListDiv = document.getElementsByClassName("notes-list")[0];
+    this.notesListDiv.setAttribute("hidden",null);
   }
 
   selectBook(book: Book) {
+    this.notesListDiv.removeAttribute("hidden");
     this.currentBook = book;
     let index = this.user.books.indexOf(book);
-    this.bookListItem = document.getElementsByClassName("book-list-item")[index];
     for (let i = 0; i < this.user.books.length; i++) {
       document.getElementsByClassName("book-list-item")[i].classList.remove("active");
     }
+    this.bookListItem = document.getElementsByClassName("book-list-item")[index];
     this.bookListItem.classList.add("active");
     console.log(book.bookTitle);
+  }
+
+  openNote(note: Note) {
+    let index = this.currentBook.notes.indexOf(note);
+    for (let i = 0; i < this.currentBook.notes.length; i++) {
+      document.getElementsByClassName("note-list-item")[i].classList.remove("active");
+    }
+    this.noteListItem = document.getElementsByClassName("note-list-item")[index];
+    this.noteListItem.classList.add("active");
+    this.noteContent = note.noteContent;
+    this.noteTitle = note.noteTitle;
+    this.newNoteInputDiv.removeAttribute("hidden");
   }
 
   createNewBook() {
@@ -56,5 +77,51 @@ export class HomeComponent implements OnInit {
   createNewNote() {
     console.log("New Note");
     this.newNoteInputDiv.removeAttribute("hidden");
+  }
+
+  saveNote() {
+    this.currentBook.notes.push({
+      noteTitle: this.noteTitle,
+      noteContent: this.noteContent 
+    });
+    this.userController.saveUser(this.user.username, this.user);
+    this.noteTitle = "";
+    this.noteContent = "";
+  }
+
+  discardNote() {
+    this.noteTitle = "";
+    this.noteContent = "";
+    this.newNoteInputDiv.setAttribute("hidden",null);
+  }
+
+  onContentChange(content : string) {
+    if(content.length > 0) {
+      this.contentChanged = true;
+    } else {
+      this.contentChanged = false;
+    }
+    if(this.contentChanged && this.titleChanged) {
+      this.saveDisabled = false;
+      this.discardDisabled = false;
+    } else {
+      this.saveDisabled = true;
+      this.discardDisabled = true;
+    }
+  }
+
+  onTitleChange(title : string) {
+    if(title.length > 0) {
+      this.titleChanged = true;
+    } else {
+      this.titleChanged = false;
+    }
+    if(this.contentChanged && this.titleChanged) {
+      this.saveDisabled = false;
+      this.discardDisabled = false;
+    } else {
+      this.saveDisabled = true;
+      this.discardDisabled = true;
+    }
   }
 }
